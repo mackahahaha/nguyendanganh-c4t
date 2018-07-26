0fb87bcc82870ce4c025dd3d290fc0ba3b86a5f1
mackahahaha <danganh.nguyen0912@gmail.com>
2018-07-26 21:03:17 +0700

    trial

---
+++ main.py
@@ -0,0 +1,18 @@
+from turtle import*
+shape("arrow")
+color("red")
+for i in range(4):
+    for i in range(2):
+
+        right(60)
+        forward(100)
+    right(120)
+    forward(100)
+    right(60)
+    forward(100)
+    left(30)
+
+
+
+
+mainloop()
